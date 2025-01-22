655ff8fd2ae6636d374ee454b432738b3fd9cc34
MIZOBUCHI Masaya <mizobuchi.masaya@gmail.com>
2025-01-22 22:31:36 +0900

    「14.システムコール」 終了時点

---
+++ 14/shell.c
@@ -0,0 +1,35 @@
+#include "user.h"
+
+void main(void) {
+#if 0
+    *((volatile int *) 0x80200000) = 0x1234;
+    for (;;);
+#endif
+
+    while (1) {
+prompt:
+        printf("> ");
+        char cmdline[128];
+        for (int i = 0;; i++) {
+            char ch = getchar();
+            putchar(ch);
+            if (i == sizeof(cmdline) -1) {
+                printf("command line too long\n");
+                goto prompt;
+            } else if (ch == '\r') {
+                printf("\n");
+                cmdline[i] = '\0';
+                break;
+            } else {
+                cmdline[i] = ch;
+            }
+        }
+
+        if  (strcmp(cmdline, "hello") == 0)
+            printf("Hello World from shell!\n");
+        else if (strcmp(cmdline, "exit") == 0)
+            exit();
+        else
+            printf("unknown command: %s\n", cmdline);
+    }
+}
