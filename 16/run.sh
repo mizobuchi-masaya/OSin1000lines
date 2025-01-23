4c6f106a8e3fed7a25f611c4d09319a6588c0a10
MIZOBUCHI Masaya <mizobuchi.masaya@gmail.com>
2025-01-23 21:59:15 +0900

    「16.ファイルシステム」 終了時点

---
+++ 16/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -xum
+
+# QEMUへのファイルパス
+QEMU=qemu-system-riscv32
+
+# clangのパス
+CC=clang
+
+OBJCOPY=/usr/bin/llvm-objcopy
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# シェルをビルド
+${CC} ${CFLAGS} -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+${OBJCOPY} --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+${OBJCOPY} -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
+# カーネルビルド
+${CC} ${CFLAGS} -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+	kernel.c common.c shell.bin.o
+
+# QEMUを起動
+## $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+##     -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+##     -drive id=drive0,file=lorem.txt,format=raw,if=none \
+##     -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
+## 	-kernel kernel.elf
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=disk.tar,format=raw,if=none \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
+	-kernel kernel.elf
---
+++ 16/shell.c
@@ -0,0 +1,43 @@
+#include "user.h"
+
+void main(void) {
+#if 0
+    *((volatile int *) 0x80200000) = 0x1234;
+    for (;;);
+#endif
+
+    while (1) {
+prompt:
+        printf("> ");
+        char cmdline[128];
+        for (int i = 0;; i++) {
+            char ch = getchar();
+            putchar(ch);
+            if (i == sizeof(cmdline) -1) {
+                printf("command line too long\n");
+                goto prompt;
+            } else if (ch == '\r') {
+                printf("\n");
+                cmdline[i] = '\0';
+                break;
+            } else {
+                cmdline[i] = ch;
+            }
+        }
+
+        if  (strcmp(cmdline, "hello") == 0)
+            printf("Hello World from shell!\n");
+        else if (strcmp(cmdline, "exit") == 0)
+            exit();
+        else if (strcmp(cmdline, "readfile") == 0) {
+            char buf[128];
+            int len = readfile("./hello.txt", buf, sizeof(buf));
+            buf[len] = '\0';
+            printf("%s\n", buf);
+        }
+        else if (strcmp(cmdline, "writefile") == 0)
+            writefile("./hello.txt", "Hello from shell!\n", 19);
+        else
+            printf("unknown command: %s\n", cmdline);
+    }
+}
